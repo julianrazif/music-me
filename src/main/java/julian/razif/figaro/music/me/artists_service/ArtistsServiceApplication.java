a17b7c194840109233e007adb474db85d45d99ea
Julian Razif Figaro <julian.razif@gmail.com>
2022-08-09 00:02:29 +0700

    add main class (SpringBootApplication)

---
+++ src/main/java/julian/razif/figaro/music/me/artists_service/ArtistsServiceApplication.java
@@ -0,0 +1,12 @@
+package julian.razif.figaro.music.me.artists_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtistsServiceApplication.class, args);
+    }
+}
