7b7708335963590084fbbad3a9774f013d2f89e5
Julian Razif Figaro <julian.razif@gmail.com>
2022-08-09 02:30:02 +0700

    enable webflux

--- src/main/java/julian/razif/figaro/music/me/artists_service/ArtistsServiceApplication.java
+++ src/main/java/julian/razif/figaro/music/me/artists_service/ArtistsServiceApplication.java
@@ -1,12 +1,25 @@
 package julian.razif.figaro.music.me.artists_service;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
+import java.util.TimeZone;
+
+@EnableWebFlux
 @SpringBootApplication
 public class ArtistsServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ArtistsServiceApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner runner() {
+        return args -> {
+            TimeZone.setDefault(TimeZone.getTimeZone("Asia/Bangkok"));
+        };
     }
 }
